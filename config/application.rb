205d4d2f8ba198800015ebb8a2466b15d4945033
chrisWohlfarth <chris.wohlfarth@gmail.com>
2016-07-24 21:41:01 +0200

    set correct time zone

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module TrakkerApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.time_zone = 'America/New_York'
   end
 end
